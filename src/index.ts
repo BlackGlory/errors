b86a8e29868a64932d9fe068c7f05f00786126db
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-02-03 03:43:53 +0000

    test: add cases

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,2 @@
-export type CustomErrorConstructor<T extends CustomError = CustomError> = new (message?: string) => T
-
-export interface SerializableError {
-  name: string
-  message: string
-  stack: string | null
-}
-
-export class CustomError extends Error {
-  get name() {
-    return this.constructor.name
-  }
-}
-
-export function normalize(err: Error): SerializableError {
-  return {
-    name: err.name
-  , message: err.message
-  , stack: err.stack ?? null
-  }
-}
+export * from './custom-error'
+export * from './normalize'
