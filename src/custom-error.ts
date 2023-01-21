ab7106df76ccfb51dba5f67a2e3808d6878ea870
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-01-21 05:33:55 +0000

    fix: remove iterable-operator dependency

--- src/custom-error.ts
+++ src/custom-error.ts
@@ -1,8 +1,7 @@
-// 由于可能出现循环引用, 禁止在本项目导入iterable-operator
-
 import { getErrorNames } from './get-error-names'
 import { isError } from './is-error'
 import { isSerializableError } from './serializable-error'
+import { first, toArray } from './utils'
 
 export type CustomErrorConstructor<T extends CustomError = CustomError> =
   new (message?: string) => T
@@ -27,14 +26,4 @@ export class CustomError extends Error {
       return false
     }
   }
-}
-
-function first<T>(iterable: Iterable<T>): T | undefined {
-  for (const element of iterable) {
-    return element
-  }
-}
-
-function toArray<T>(iterable: Iterable<T>): T[] {
-  return Array.from(iterable)
 }
--- src/normalize.ts
+++ src/normalize.ts
@@ -1,6 +1,6 @@
 import { getErrorNames } from './get-error-names'
-import { toArray } from 'iterable-operator'
 import { SerializableError } from './serializable-error'
+import { toArray } from './utils'
 
 export function normalize(err: Error): SerializableError {
   const [name, ...ancestors] = toArray(getErrorNames(err))
