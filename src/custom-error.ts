857f19fdcf34177930214533771fe5fa2ee3640b
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2023-01-21 05:29:54 +0000

    fix: remove iterable-operator dependency

--- src/custom-error.ts
+++ src/custom-error.ts
@@ -1,4 +1,5 @@
-import { first, toArray } from 'iterable-operator'
+// 由于可能出现循环引用, 禁止在本项目导入iterable-operator
+
 import { getErrorNames } from './get-error-names'
 import { isError } from './is-error'
 import { isSerializableError } from './serializable-error'
@@ -26,4 +27,14 @@ export class CustomError extends Error {
       return false
     }
   }
+}
+
+function first<T>(iterable: Iterable<T>): T | undefined {
+  for (const element of iterable) {
+    return element
+  }
+}
+
+function toArray<T>(iterable: Iterable<T>): T[] {
+  return Array.from(iterable)
 }
